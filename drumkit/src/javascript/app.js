33b1a333f3c8b296b423f3d680cdbbda99ae78ed
Juan Robles <juansrx@gmail.com>
2024-11-28 01:00:40 -0500

    feat: Added 0.A000-Regular.ttf feat: Added layout for the keys feat: Event handling for adding and removed the transformation css rule for the keys

--- drumkit/src/javascript/app.js
+++ drumkit/src/javascript/app.js
@@ -1,11 +1,37 @@
+/**
+ * 
+ * @param {*} e 
+ */
+function handleTransitionEnd(e) {
+
+    if (e.propertyName === 'transform')
+        this.classList.remove('playing');
+}
+
+const drumKeys = document.querySelectorAll('.drum-keys .key');
+
+console.log(drumKeys);
+
+drumKeys.forEach((drumKey) => {
+    drumKey.addEventListener('transitionend', handleTransitionEnd);
+});
+
 window.addEventListener('keydown', function (event) {
-    
+
     const audioFile = document.querySelector(`audio[data-key="${event.key}"]`);
 
     if (audioFile) {
-        
         audioFile.currentTime = 0;
         audioFile.play();
+
+        for (let drumKey of drumKeys) {
+
+            if (drumKey.attributes['data-key'].value === event.key) {
+
+                drumKey.classList.add('playing');
+                break;
+            }
+        }
     }
 
 });
