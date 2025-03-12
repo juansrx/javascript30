37c2e7a64ff638855e087639c05afeb43e21ae54
Juan Robles <juansrx@gmail.com>
2025-03-11 21:47:09 -0500

    feat: styles.css created to add css feat: generateRecipeHTML adds highlights to instances of the search term on every matching recipe

--- typeahead/src/javascript/app.js
+++ typeahead/src/javascript/app.js
@@ -24,29 +24,32 @@ const filterByWord = (word) => {
  * @param {*} recipe 
  * @returns 
  */
-const generateRecipeHTML = (recipe) => {
+const generateRecipeHTML = (recipe, word) => {
+
+    const regexp = new RegExp(word, 'gi');
 
     return `<li class="recipeItem">
         <h3 class="name">
-            ${recipe.name}
+            ${recipe.name.replace(regexp, `<span class="highlight">${word}</span>`)}
         </h3>
         <div class="cuisine">
-            ${recipe.cuisine}
-        <div/>
+            ${recipe.cuisine.replace(regexp, `<span class="highlight">${word}</span>`)}
+        </div>
         <div class="ingredients">
-            ${recipe.ingredients.map(ingredient =>'<span>' + ingredient + '</span>').join(' ')}
+            ${recipe.ingredients.map(ingredient =>'<span>' + ingredient.replace(regexp, `<span class="highlight">${word}</span>`) + '</span>').join(' ')}
         </div>
     </li>`;
 };
 
 
+/**
+ * 
+ */
 const displayMatchesList = function () {
 
     const matchesHTML = filterByWord(this.value).map(
-        recipe => generateRecipeHTML(recipe)
+        recipe => generateRecipeHTML(recipe, this.value)
     );
-
-    console.log(matchesHTML);
 
     document.querySelector('#recipeList').innerHTML = matchesHTML.join("\n");
 };
